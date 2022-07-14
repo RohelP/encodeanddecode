d3872bd8fbf7f58ecfb8520e2313e8a1a2c2f162
RohelP <pereirarohel21@gmail.com>
2022-07-14 00:01:19 -0400

    main

--- encode.py
+++ encode.py
@@ -1,26 +1,12 @@
+'''
+Rohel Pereira
+200668360
+'''
+
+
 givenstring = " ,.0123456789abcdefghijklmnopqrstuvwxyz"
 tab = {}
 arr = []
-
-for num in givenstring:
-    tab[num] = 0
-with open('test1.txt', 'r') as fileinput:
-    for x in fileinput:
-        x = x.lower()
-        for num in x:
-            if num.isspace():
-                tab[" "] += 1
-            elif num.isalnum() or num == "." or num ==",":
-                tab[num] += 1
-
-f = open("frequency.txt", "w")
-for key in tab:
-    if key == "z":
-        f.write(key+":"+str(tab[key]))
-    else:
-        f.write(key+":"+str(tab[key])+"\n")
-f.close()
-
 
 class NodeTree():
 
@@ -28,16 +14,41 @@ class NodeTree():
         self.right = right
         self.left = left
 
+for sym in givenstring:
+    tab[sym] = 0
+
+with open('test1.txt', 'r') as fileinput:
+    for line in fileinput:
+        line = line.lower()
+
+        for sym in line:
+            if sym.isspace():
+                tab[" "] += 1
+            elif sym.isalnum() or sym == "." or sym ==",":
+                tab[sym] += 1
+
+f = open("frequency.txt", "w")
+for x in tab:
+    if x == "z":
+        f.write(x+":"+str(tab[x]))
+    else:
+        f.write(x+":"+str(tab[x])+"\n")
+
+f.close()
+
 def huffman_code_tree(node, left=True, binString=''):
     if type(node) is str:
         return {node: binString}
+
     dictionary = dict()
     l = node.left
     r = node.right 
+
     huff = huffman_code_tree(l, True, binString + '1')
     dictionary.update(huff)
     huff = huffman_code_tree(r, False, binString + '0')
     dictionary.update(huff)
+    
     return dictionary
 
 
@@ -56,26 +67,26 @@ while len(link) > 1:
 huffmanCode = huffman_code_tree(link[0][0])
 
 f = open("codes.txt", "w")
-for (num, frequency) in freq:
-    if num == freq[-1][0]:
-        f.write(num + ":" + huffmanCode[num])
+for (sym, frequency) in freq:
+    if sym == freq[-1][0]:
+        f.write(sym + ":" + huffmanCode[sym])
     else:
-        f.write(num + ":" + huffmanCode[num] + '\n')
+        f.write(sym + ":" + huffmanCode[sym] + '\n')
 
 f.close()
 
-
-decode = open("compressed.bin", "wb")
+d = open("compressed.bin", "wb")
 with open('test1.txt', 'r') as fileinput:
-    for x in fileinput:
-        x = x.lower()
-        for num in x:
+    for loop in fileinput:
+        loop = loop.lower()
+        for num in loop:
             if num in huffmanCode:
-                arr+=huffmanCode[num]
+                arr += huffmanCode[num]
         
+bins="".join(map(str,arr))
+byt = bytearray(int(bins[count:count+8], 2) 
 
-b="".join(map(str,arr))
-y = bytearray(int(b[x:x+8], 2) 
-for x in range(0, len(b), 8))
-decode.write(y)
-decode.close()
+for count in range(0, len(bins), 8))
+d.write(byt)
+
+d.close()
